c1cb74b4dfc2f787a1a19370e886384aff796dfb
Michael Gregoire <mdgregoire@gmail.com>
2018-04-30 09:25:20 -0500

    added more dev mode

--- server/server.js
+++ server/server.js
@@ -21,6 +21,7 @@ app.use(passport.session());
 //if you are on ocalhost you need to create your own ssl certificate
 //if you are on heroku you need to not use the ssl cert since heroku provides its own
 if(process.env.DEV) {
+  console.log('in DEV mdoe');
   //pem generates our SSL Certifiace here
   pem.createCertificate({ days: 1, selfSigned: true }, function (err, keys) {
     if (err) {
@@ -31,6 +32,7 @@ if(process.env.DEV) {
   console.log('listening on port 4430');
   })
 } else{
+  console.log('in live mode');
   //if you are not in dev mode the server does not need to generate its own ssl
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, function(){
